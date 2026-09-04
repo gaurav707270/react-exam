28ca04fb7c81665e374f72ab16ae09ca4c60ba43
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-04 23:17:52 +0530

    Update Navbar.jsx

--- student-app/src/components/Navbar.jsx
+++ student-app/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 // const authSlice = createSlice({
     name: "auth",
     // initialState: {
-        // user: {
+        user: {
             // email: "",
             // password: "",
         }
