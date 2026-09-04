20506386c40aca5792e0769f9776156b7b8ff0f8
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-04 23:18:14 +0530

    Update Navbar.jsx

--- student-app/src/components/Navbar.jsx
+++ student-app/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const authSlice = createSlice({
     name: "auth",
     initialState: {
         user: {
-            // email: "",
+            email: "",
             password: "",
         }
     // },
