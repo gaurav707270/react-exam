c0940b01a75c8362532d3154e039a0176f9397f7
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-23 19:15:45 +0530

    Update Navbar.jsx

--- student-app/src/components/Navbar.jsx
+++ student-app/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const authSlice = createSlice({
     },
     reducers: {
         singUp: (state, action) => {
-            // user.state = action.payload
+            user.state = action.payload
 
         },
 
